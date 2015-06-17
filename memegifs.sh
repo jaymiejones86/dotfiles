a46b3862c2862d738584d8445f156d791b2ffe25
Jaymie Jones <jaymiejones86@gmail.com>
2015-06-18 07:47:39 +1000

    Updated Gif Paths

--- memegifs.sh
+++ memegifs.sh
@@ -2,32 +2,48 @@
 # Bash Menu Script Example
 
 PS3='Please enter your choice: '
-options=("high five" "bugs are coming" "feature bug" "mindblown" "somuchwin" "userissue" "Quit")
+options=("high five" "bugs are coming" "feature bug" "mindblown" "somuchwin" "userissue" "shootself" "shoot" "dwightshrutecelebrating" "ace v dancing" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "high five")
-            echo 'https://p.gr-assets.com/540x540/fit/hostedimages/1380393870/758402.gif' | pbcopy | printf 'Copied highest of fives to pasteboard.'
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/highestfive.gif' | pbcopy | printf 'Copied highest of fives to pasteboard.'
             break
             ;;
         "bugs are coming")
-            echo 'http://www.troll.me/images/gandolf/the-bugs-are-comming-you-shall-not-pass-thumb.jpg' | pbcopy | printf 'Copied bugs are coming to pasteboard.'
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/the-bugs-are-comming-you-shall-not-pass-thumb.jpg' | pbcopy | printf 'Copied bugs are coming to pasteboard.'
             break
             ;;
         "feature bug")
-            echo 'http://cdn.memegenerator.net/instances/250x250/33018175.jpg' | pbcopy | printf 'Copied feature bugs to pasteboard.'
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/feature-bug.jpg' | pbcopy | printf 'Copied feature bugs to pasteboard.'
             break
             ;;
         "mindblown")
-            echo 'http://img.photobucket.com/albums/v201/foolishtook/HIMYM/mindblown.gif' | pbcopy | printf 'Copied mind blown to pasteboard.'
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/mindblown.gif' | pbcopy | printf 'Copied mind blown to pasteboard.'
             break
             ;;
         "somuchwin")
-            echo 'http://www.quickmeme.com/img/0f/0ff49113f737f6725161e579f345863f0a8b6d82d260aca7a3726c18b760b024.jpg' | pbcopy | printf 'Copied so much win to pasteboard.'
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/somuchwin.jpg' | pbcopy | printf 'Copied so much win to pasteboard.'
             break
             ;;
         "userissue")
-            echo 'http://s3-ap-southeast-1.amazonaws.com/jaymiejones86-settings/images/seeusers.gif' | pbcopy | printf 'Copied user issue to pasteboard.'
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/seeusers.gif' | pbcopy | printf 'Copied user issue to pasteboard.'
+            break
+            ;;
+        "shootself")
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/shootself.gif' | pbcopy | printf 'Copied user issue to pasteboard.'
+            break
+            ;;
+        "dwightshrutecelebrating")
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/dwightshrutecelebrating.gif' | pbcopy | printf 'Copied user issue to pasteboard.'
+            break
+            ;;
+        "shoot")
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/shoot.gif' | pbcopy | printf 'Copied user issue to pasteboard.'
+            break
+            ;;
+        "ace v dancing")
+            echo 'http://s3-ap-southeast-1.amazonaws.com/pixelstack-dev/gifs/ace-ventura-dancing.gif' | pbcopy | printf 'Copied user issue to pasteboard.'
             break
             ;;
         "Quit")
