68f6cf90277aa09693e5f466918b884d5f211f36
Jaymie Jones <jaymiejones86@gmail.com>
2023-12-20 13:51:49 +1100

    Update Lua Config for Ident Lines Due to Plugin Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,15 +147,8 @@ require('lazy').setup({
     },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+  -- Add indentation guides even on blank lines
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
