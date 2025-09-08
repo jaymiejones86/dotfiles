23c92c697d43f2f639924915f132e8edb9c657c7
Jaymie Jones <jaymiejones86@gmail.com>
2025-09-09 09:26:50 +1000

    Add Lazy Git in Floating Terminal
    Adds Lazygit as a floating terminal using the <leader>lg keymap

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -29,6 +29,29 @@ return {
   "tpope/vim-obsession", -- Session management
   "mbbill/undotree", -- Undo tree visualization
   "kdheepak/lazygit.nvim", -- LazyGit integration
+
+  -- Git Management
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+
   
   -- Themes
   { 
