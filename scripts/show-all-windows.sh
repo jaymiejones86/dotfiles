b1499a82fef3a0c8db01c21bc5efcb3467126e91
Jaymie Jones <jaymiejones86@gmail.com>
2025-09-18 15:27:09 +1000

    Add Scripts for Managing Mac OS to Assist with Mouse Config

---
+++ scripts/show-all-windows.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Script to show all windows (Mission Control) on macOS
+osascript -e 'tell application "System Events" to key code 160'
---
+++ scripts/show-desktop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Script to show desktop on macOS (F11)
+osascript -e 'tell application "System Events" to key code 103'
